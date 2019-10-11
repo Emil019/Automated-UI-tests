8239c30d6e2ff35c0023e52b922c0889c8a73b83
abdrahmanov <abdrahmanov@mjr.ru>
2019-10-11 15:41:22 +0300

    11.10

---
+++ UnitTestProject1/UnitTestProject1/model/ContactData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class ContactData
+    {
+        private string firstname;
+        private string lastname = "";
+        private string mobile = "";
+        private string email = "";
+        private string address = "";
+
+        public ContactData(string name)
+        {
+            this.firstname = name;
+        }
+
+        
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+
+        }
+        public string Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+            set
+            {
+                mobile = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
